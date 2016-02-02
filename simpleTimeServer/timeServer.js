8693f7787762d27ee1a12ab88e2baa0583b5ffe2
comraq <yinadam@hotmail.com>
2016-02-01 17:53:35 -0800

    adding a simple time server in node

---
+++ simpleTimeServer/timeServer.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app = express();
+
+app.get("*", function(req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+
+  var time = new Date(req.url.split("%20").join(" ").slice(1));
+  var reply = {
+    unix: (time == "Invalid Date")? null : time.getTime(),
+    natural: (time == "Invalid Date")? null : time.toDateString()
+  };
+  res.end(JSON.stringify(reply));
+});
+app.listen(7777);
