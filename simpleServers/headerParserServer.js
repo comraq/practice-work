68b88c61712c5d565a5103bc70898e311041be13
comraq <yinadam@hotmail.com>
2016-02-02 20:06:59 -0800

    added a simple header parsing server

---
+++ simpleServers/headerParserServer.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app = express();
+
+app.get("*", function(req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  var reply = {
+    ipaddress: req.ip.split(":").pop(),
+    language: req.headers["accept-language"].split(",")[0],
+    device: req.headers["user-agent"].match(/\([^\)]*\)/)[0].slice(1, -1)
+  };
+  res.end(JSON.stringify(reply));
+});
+app.listen(7777);
