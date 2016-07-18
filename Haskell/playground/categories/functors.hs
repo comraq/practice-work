caf2762c5d8ad768bc0d9aacea87c4dc932370e6
adam <adam.yin@pillrcompany.com>
2016-07-17 21:48:28 -0400

    finishing functors/applicatives/monoids

--- Haskell/playground/categories/functors.hs
+++ Haskell/playground/categories/functors.hs
@@ -1,6 +1,7 @@
 import Data.Char
 import Data.List
 import Control.Applicative
+import Data.Monoid
 
 main = do
   line <- mymap (bang . intersperse '-' . reverse . map toUpper) getLine
@@ -69,5 +70,109 @@ instance MyAppli ZipList where
   mypure                         = ZipList . repeat
   myap (ZipList fs) (ZipList xs) = ZipList $ zipWith (\f x -> f x) fs xs
 
-sequenceA :: (Applicative f) => [f a] -> f [a]
-sequenceA =  foldr (liftA2 (:)) (pure [])
+mySeqA :: (Applicative f) => [f a] -> f [a]
+mySeqA =  foldr (liftA2 (:)) (pure [])
+
+newtype CharList = CharList { getCharList :: [Char] } deriving (Eq, Show)
+
+newtype Pair b a = Pair { getPair :: (a, b) }
+
+instance Functor (Pair c) where
+  fmap f (Pair (x, y)) = Pair (f x, y)
+
+newtype CoolBool = CoolBool { getCoolBool :: Bool }
+
+helloMe              :: CoolBool -> String
+helloMe (CoolBool _) =  "hello"
+
+--lengthCompare     :: String -> String -> Ordering
+--lengthCompare x y =  (length x `compare` length y) `myAppend`
+--                     (vowels x `compare` vowels y) `myAppend`
+--                     (x `compare` y)
+--  where vowels = length . filter (`elem` "aeiou")
+
+--class MyMonoid m where
+--  myEmpty  :: m
+--  myAppend :: m -> m -> m
+--
+--  myConcat :: [m] -> m
+--  myConcat =  foldr myAppend myEmpty
+--
+--instance MyMonoid [a] where
+--  myEmpty  = []
+--  myAppend = (++)
+--
+--newtype Product a = Product { getProduct :: a }
+--  deriving (Eq, Ord, Read, Show, Bounded)
+--
+--newtype Sum a = Sum { getSum :: a }
+--  deriving (Eq, Ord, Read, Show, Bounded)
+--
+--newtype Any = Any { getAny :: Bool }
+--  deriving (Eq, Ord, Read, Show, Bounded)
+--
+--newtype All = All { getAll :: Bool }
+--  deriving (Eq, Ord, Read, Show, Bounded)
+--
+--instance Num a => MyMonoid (Product a) where
+--  myEmpty                          = Product 1
+--  myAppend (Product x) (Product y) = Product $ x * y
+--
+--instance Num a => MyMonoid (Sum a) where
+--  myEmpty                  = Sum 0
+--  myAppend (Sum x) (Sum y) = Sum $ x + y
+--
+--instance MyMonoid Any where
+--  myEmpty                  = Any False
+--  myAppend (Any x) (Any y) = Any $ x || y
+--
+--instance MyMonoid All where
+--  myEmpty                  = All True
+--  myAppend (All x) (All y) = All $ x && y
+--
+--instance MyMonoid Ordering where
+--  myEmpty       = EQ
+--  myAppend LT _ = LT
+--  myAppend EQ y = y
+--  myAppend GT _ = GT
+--
+--instance MyMonoid a => MyMonoid (Maybe a) where
+--  myEmpty                      = Nothing
+--  myAppend Nothing m           = m
+--  myAppend m Nothing           = m
+--  myAppend (Just m1) (Just m2) = Just $ myAppend m1 m2
+--
+--newtype First a = First { getFirst :: Maybe a }
+--  deriving (Eq, Ord, Read, Show)
+--
+--instance MyMonoid (First a) where
+--  myEmpty                     = First Nothing
+--  myAppend (First (Just x)) _ = First $ Just x
+--  myAppend (First Nothing) x  = x
+--
+--newtype Last a = Last { getLast :: Maybe a }
+--  deriving (Eq, Ord, Read, Show)
+--
+--instance MyMonoid (Last a) where
+--  myEmpty                    = Last Nothing
+--  myAppend _ (Last (Just x)) = Last $ Just x
+--  myAppend x (Last Nothing)  = x
+
+data Tree a = Leaf | Node a (Tree a) (Tree a)
+  deriving (Show, Read, Eq)
+
+instance Foldable Tree where
+  foldMap f Leaf = mempty
+  foldMap f (Node x l r) = foldMap f l `mappend`
+                           f x         `mappend`
+                           foldMap f r
+
+testTree = Node 5
+             (Node 3
+               (Node 1 Leaf Leaf)
+               (Node 6 Leaf Leaf)
+             )
+             (Node 9
+               (Node 8 Leaf Leaf)
+               (Node 10 Leaf Leaf)
+             )
