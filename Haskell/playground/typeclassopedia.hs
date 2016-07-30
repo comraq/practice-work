cc54522a585f09dc85f8dedbbf5199e1cf774599
adam <adam.yin@pillrcompany.com>
2016-07-29 23:17:27 -0400

    finishing typeclassopedia

--- Haskell/playground/typeclassopedia.hs
+++ Haskell/playground/typeclassopedia.hs
@@ -326,16 +326,73 @@ class Monad' m => MonadPlus' m where
  - v >> mzero  === mzero
  -}
 
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE TypeOperators #-}
+class Category' cat' where
+  id'   :: cat' a a
+  comp' :: cat' b c -> cat' a b -> cat' a c
 
-type Arrow' = A.Arrow
+newtype Kleisli' m a b = Kleisli' { runKleisli :: a -> m b }
 
-class Arrow' arr => ArrowZero' arr where
-  zeroArrow' :: (b `arr` c)
+instance Monad' m => Category' (Kleisli' m) where
+  id'                           = Kleisli' return'
+  Kleisli' a `comp'` Kleisli' b = Kleisli' $ \c -> b c `bind'` \b' -> a b'
 
-class ArrowZero' arr => ArrowPlus' arr where
-  arrowChoice' :: (b `arr` c) -> (b `arr` c) -> (b `arr` c)
+class Category' arr' => Arrow' arr' where
+  arr'      :: (b -> c) -> (arr' b c)
+
+  first'    :: (arr' b c) -> (arr' (b, d) (c, d))
+  first'    =  flip parallel' id'
+
+  second'   :: (arr' b c) -> (arr' (d, b) (d, c))
+  second'   =  parallel' id'
+
+  parallel'     :: (arr' b c) -> (arr' b' c') -> (arr' (b, b') (c, c'))
+  parallel' f g =  arr' swap `comp'` first' g `comp'` arr' swap `comp'` first' f
+    where swap (x, y) = (y, x)
+
+  fanout'     :: (arr' b c) -> (arr' b c') -> (arr' b (c, c'))
+  fanout' f g =  parallel' f g `comp'` arr' (\b -> (b, b)) 
+
+{-
+ - parallel' :: (arr' b c) -> (arr' b' c') -> (arr' (b, b') (c, c'))
+ - parallel' :: (b -> c) -> (b' -> c') -> ((b, b') -> (c, c'))
+ -
+ - parallel' f g = arr' swap `comp'` first' g `comp'` arr' swap `comp'` first' f
+ -   where swap (x, y) = (y, x)
+ -
+ - (parallel' f g) (b, b')
+ - (arr' swap `comp'` first' g `comp'` arr' swap `comp'` first' f) (b, b')
+ - (arr' swap `comp'` first' g `comp'` arr' swap) (f b, b')
+ - (arr' swap `comp'` first' g `comp'` arr' swap) (c, b')
+ - (arr' swap `comp'` first' g) (b', c)
+ - (arr' swap) (g b', c)
+ - (arr' swap) (c', c)
+ - (c, c')
+ -}
+
+{-
+ - Arrow Laws
+ -
+ - 1) arr id === id
+ -
+ - 2) arr (h . g) === arr g >> arr h
+ -
+ - 3) first $ arr g === arr (g *** id)
+ -
+ - 4) first (g >>> h) === first g >>> first h
+ -
+ - 5) first g >>> arr (id *** h) === arr (id *** h) >>> first g
+ -
+ - 6) first g >>> arr fst === arr fst >>> g
+ -
+ - 7) first (first g) >>> arr assoc === arr assoc >>> first g
+ -      where assoc ((x, y), z) = (x, (y, z))
+ -}
+
+class Arrow' arr' => ArrowZero' arr' where
+  zeroArrow' :: (b `arr'` c)
+
+class ArrowZero' arr' => ArrowPlus' arr' where
+  arrowChoice' :: (b `arr'` c) -> (b `arr'` c) -> (b `arr'` c)
 
 class Foldable' t where
   fold'    :: Monoid' m => t m -> m
@@ -359,3 +416,179 @@ instance Foldable' Tree' where
   foldMap' f Empty'        = unit'
   foldMap' f (Leaf' x)     = f x
   foldMap' f (Node' l k r) = foldMap' f l <> f k <> foldMap' f r
+
+{-
+ - foldMap . foldMap :: (Foldable t, Foldable t', Monoid m) => (a -> m) -> t' (t a) m
+ -
+ - foldMap . foldMap .foldMap ::
+ -   (Foldable t, Foldable t', Foldable t'', Monoid m) => (a -> m) -> t'' (t' (t a)) m
+ -
+ - Traverses into any nested Foldable, maps its contents to a monoid and
+ - folds down the resulting monoid
+ -}
+
+instance Semigroup' [a] where
+  (<>) = (++)
+
+instance Monoid' [a] where
+  unit' = []
+
+toList' :: (Foldable' t) => t a -> [a]
+toList' =  foldr'' (:) []
+
+concatF' :: (Foldable' t) => t [a] -> [a]
+concatF' =  fold'
+
+concatMapF' :: (Foldable' t) => (a -> [b]) -> t a -> [b]
+concatMapF' =  foldMap'
+
+newtype And' = And' { runAnd' :: Bool }
+newtype Or'  = Or' { runOr' :: Bool }
+
+instance Semigroup' And' where
+  And' a <> And' b = And' $ a && b
+
+instance Semigroup' Or' where
+  Or' a <> Or' b = Or' $ a || b
+
+instance Monoid' And' where
+  unit' = And' True
+
+instance Monoid' Or' where
+  unit' = Or' False
+
+class (Functor' t, Foldable' t) => Traversable' t where
+  traverse'   :: (Applicative' f) => (a -> f b) -> t a -> f (t b)
+  traverse' f =  sequenceA' . fmap' f
+
+  sequenceA' :: (Applicative' f) => t (f a) -> f (t a)
+  sequenceA' =  traverse' id
+
+  mapM' :: (Monad' m) => (a -> m b) -> t a -> m (t b)
+  mapM' =  traverse'
+
+  sequence' :: (Monad' m) => t (m a) -> m (t a)
+  sequence' =  sequenceA'
+
+{-
+ - Tree [a] -> [Tree a]
+ -   traverse id
+ -   sequenceA
+ -
+ - [Tree a] -> Tree [a]
+ -
+ - traverse . traverse :: (Applicative f, Traversable t, Traversable t') =>
+ -   (a -> f b) -> t (t' a) -> f (t (t' b))
+ -
+ - Maps the value of a Traversable from a -> b, if b yields effects
+ - (Applicative f), then the entire Traversable is encapsulated with
+ - Applicative f
+ -}
+
+instance Functor' Tree' where
+  fmap' _ Empty'        = Empty'
+  fmap' f (Leaf' x)     = Leaf' $ f x
+  fmap' f (Node' l k r) = Node' (fmap' f l) (f k) (fmap' f r)
+
+instance Traversable' Tree' where
+  traverse' g Empty'        = pure' Empty'
+  traverse' g (Leaf' x)     = Leaf' `fmap'` g x
+  traverse' g (Node' l k r) = Node' `fmap'` traverse' g l `ap'` g k `ap'` traverse' g r
+
+newtype Id' a = Id' { runId' :: a }
+
+instance Functor' Id' where
+  fmap' f (Id' a) = Id' $ f a
+
+instance Applicative' Id' where
+  pure'                 = Id'
+  (Id' f) `ap'` (Id' a) = Id' $ f a
+
+newtype Const' a b = Const' { runConst' :: a }
+
+instance Functor' (Const' a) where
+  fmap' _ (Const' a) = Const' a
+
+instance Monoid' m => Applicative' (Const' m) where
+  pure' _                 = Const' unit'
+  Const' f `ap'` Const' a = Const' $ (<>) f a
+
+fmapT'     :: (Traversable' t) => (a -> b) -> t a -> t b
+fmapT' f a =  runId' $ traverse' (Id' . f) a
+
+foldMapT'     :: (Traversable' t, Monoid' m) => (a -> m) -> t a -> m
+foldMapT' f a =  runConst' $ traverse' (Const' . f) a
+
+{-
+ - Traverable Laws
+ -
+ - 1) traverse Identity === Identity
+ -    ( traversals cannot make up arbitrary effects )
+ -
+ - 2) traverse (Compose . fmap g . f) === Compose . fmap (traverse g) . traverse f
+ -
+ - 3) eta :: forall a f g. (Applicative f, Applicative g) => f a -> g a
+ -    ex: eta (pure x) === pure x
+ -        eta (x <*> y) === eta x <*> eta y
+ -
+ -    eta . traverse f === traverse (eta . f)
+ -}
+
+class Arrow' arr' => ArrowChoice' arr' where
+  left'      :: (b `arr'` c) -> (Either b d `arr'` Either c d)
+  right'     :: (b `arr'` c) -> (Either d b `arr'` Either d c)
+  parallelC' :: (b `arr'` c) -> (b' `arr'` c') -> (Either b b' `arr'` Either c c')
+  fanoutC'   :: (b `arr'` d) -> (c `arr'` d) -> (Either b c `arr'` d)
+
+class Arrow' arr' => ArrowApply' arr' where
+  app' :: (b `arr'` c, b) `arr'` c
+
+instance Monad' m => Arrow' (Kleisli' m) where
+  arr' f               = Kleisli' $ return' . f
+  first' (Kleisli' f)  = Kleisli' $ \(b, d) -> f b `bind'` \c -> return' (c, d)
+  second' (Kleisli' f) = Kleisli' $ \(d, b) -> f b `bind'` \c -> return' (d, c)
+
+instance Monad' m => ArrowApply' (Kleisli' m) where
+  app' = Kleisli' (\(Kleisli' f, x) -> f x)
+
+newtype ArrowMonad' a b = ArrowMonad' (a () b)
+
+instance Arrow' a => Functor' (ArrowMonad' a) where
+  fmap' f (ArrowMonad' m) = ArrowMonad' $ arr' f `comp'` m
+
+instance Arrow' a => Applicative' (ArrowMonad' a) where
+  pure' x                           = ArrowMonad' (arr' $ const x)
+  ArrowMonad' f `ap'` ArrowMonad' a = ArrowMonad' (arr' (uncurry id) `comp'` fanout' f a)
+
+instance ArrowApply' a => Monad' (ArrowMonad' a) where
+  return' = pure'
+  (ArrowMonad' a) `bind'` k = ArrowMonad' $
+    app' `comp'` arr' (\x -> let ArrowMonad' h = k x
+                             in  (h, ()))
+         `comp'` a
+
+class Arrow' a => ArrowLoop' a where
+  loop' :: a (b, d) (c, d) -> a b c
+
+trace'     :: ((b, d) -> (c, d)) -> b -> c
+trace' f b =  let (c, d) = f (b, d) in c
+
+class ArrowLoop' arr' => ArrowCircuit' arr' where
+  delay' :: b -> (b `arr'` b)
+
+class Functor' w => Comonad' w where
+  extract :: w a -> a
+
+  duplicate :: w a -> w (w a)
+  duplicate =  extend id
+
+  extend   :: (w a -> b) -> w a -> w b
+  extend f =  fmap' f . duplicate
+
+data Stream' a = Cons' a (Stream' a)
+
+instance Functor' Stream' where
+  fmap' f (Cons' x xs) = Cons' (f x) $ fmap' f xs
+
+instance Comonad' Stream' where
+  extract (Cons' x _) = x
